a45d380dfb66396847be82801562da7501bbe136
Michael VanScoyk <michael.vanscoyk@gmail.com>
2026-03-08 14:39:51 -0400

    Production cleanup: remove dead patches, backups, ADI logs, merge artifacts, and junk files

--- analytics_implementation/frontend/src/components/Analytics/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Analytics Components Index
- */
-
-export { default as AnalyticsChart } from './AnalyticsChart';
-export { default as QueryInterface } from './QueryInterface';
-export { default as ResultsTable } from './ResultsTable';
-export { default as PredictionsPanel } from './PredictionsPanel';
-export { default as DataQualityPanel } from './DataQualityPanel';
